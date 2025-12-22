6051d5324268885a3d1b70df9a657d345238e54d
Michele Andreoli <m.andreoli@mindicity.com>
2025-12-22 19:37:41 +0000

    style: 💄 linter

--- eslint.config.js
+++ eslint.config.js
@@ -204,7 +204,7 @@ module.exports = [
       // Relaxed rules for test files
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['warn', { 
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_' 
