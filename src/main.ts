b4f34a7ad6c80b0484db90261158c820c4cf6a38
Michele Andreoli <m.andreoli@mindicity.com>
2026-01-09 08:16:13 +0000

    fix: 🐛 lint

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-import { ZodValidationPipe } from 'nestjs-zod';
 
 import fastifyCompress from '@fastify/compress';
 import helmet from '@fastify/helmet';
 import { config } from 'dotenv';
 import { Logger } from 'nestjs-pino';
+import { ZodValidationPipe } from 'nestjs-zod';
 import { v4 as uuidv4 } from 'uuid';
 
 import { AppModule } from './app.module';
